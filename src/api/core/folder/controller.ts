9d056af3f33eb23ae03b7ffc3ca744ba5130aa82
Dr-Greg <grigor.mouradyan@outlook.fr>
2025-01-07 14:09:05 +0100

    refactor: remove try catch blocks from services and controllers

--- src/api/core/folder/controller.ts
+++ src/api/core/folder/controller.ts
@@ -1,5 +1,4 @@
 import { InternalServerError, NotFound } from "@/helpers/HttpError";
-import logger from "@/helpers/logger";
 import { AppRouteHandler } from "@/types";
 
 import { Create, Find, FindOne, Remove, Update } from "./route";
@@ -7,78 +6,53 @@ import FolderService from "./service";
 
 export default abstract class FolderController {
   public static create: AppRouteHandler<Create> = async (c) => {
-    try {
-      const data = c.req.valid("json");
-      const res = await FolderService.create(data);
+    const data = c.req.valid("json");
+    const res = await FolderService.create(data);
 
-      if (!res) {
-        throw new InternalServerError("Folder not created");
-      }
-
-      return c.json(res, 201);
-    } catch (error) {
-      logger.error("[FolderController.create] - unexpected error");
-      throw error;
+    if (!res) {
+      throw new InternalServerError("Folder not created");
     }
+
+    return c.json(res, 201);
   };
 
   public static find: AppRouteHandler<Find> = async (c) => {
-    try {
-      const res = await FolderService.find();
+    const res = await FolderService.find();
 
-      return c.json(res, 200);
-    } catch (error) {
-      logger.error("[FolderController.find] - unexpected error");
-      throw error;
-    }
+    return c.json(res, 200);
   };
 
   public static findOne: AppRouteHandler<FindOne> = async (c) => {
-    try {
-      const { folderId } = c.req.valid("param");
-      const res = await FolderService.findOne(folderId);
+    const { folderId } = c.req.valid("param");
+    const res = await FolderService.findOne(folderId);
 
-      if (!res) {
-        throw new NotFound("Folder not found");
-      }
-
-      return c.json(res, 200);
-    } catch (error) {
-      logger.error("[FolderController.findOne] - unexpected error");
-      throw error;
+    if (!res) {
+      throw new NotFound("Folder not found");
     }
+
+    return c.json(res, 200);
   };
 
   public static update: AppRouteHandler<Update> = async (c) => {
-    try {
-      const { folderId } = c.req.valid("param");
-      const data = c.req.valid("json");
-      const res = await FolderService.update(folderId, data);
+    const { folderId } = c.req.valid("param");
+    const data = c.req.valid("json");
+    const res = await FolderService.update(folderId, data);
 
-      if (!res) {
-        throw new NotFound("Folder not found");
-      }
-
-      return c.json(res, 200);
-    } catch (error) {
-      logger.error("[FolderController.update] - unexpected error");
-      throw error;
+    if (!res) {
+      throw new NotFound("Folder not found");
     }
+
+    return c.json(res, 200);
   };
 
   public static remove: AppRouteHandler<Remove> = async (c) => {
-    try {
-      const { folderId } = c.req.valid("param");
-      const res = await FolderService.remove(folderId);
+    const { folderId } = c.req.valid("param");
+    const res = await FolderService.remove(folderId);
 
-      if (!res) {
-        throw new NotFound("Folder not found");
-      }
-
-      return c.json(res, 200);
-    } catch (error) {
-      logger.error("[FolderController.remove] - unexpected error");
-      throw error;
+    if (!res) {
+      throw new NotFound("Folder not found");
     }
+
+    return c.json(res, 200);
   };
 }
--- src/api/core/folder/service.ts
+++ src/api/core/folder/service.ts
@@ -2,88 +2,62 @@ import { eq } from "drizzle-orm";
 
 import db from "@/database/database";
 import { Folder, folder } from "@/database/schemas/folder";
-import logger from "@/helpers/logger";
 
 export default abstract class FolderService {
   public static async create(data: Folder["Insert"]) {
-    try {
-      const folderCreated = await db.insert(folder).values(data).returning();
+    const folderCreated = await db.insert(folder).values(data).returning();
 
-      if (!folderCreated.length) {
-        return null;
-      }
-
-      return folderCreated[0];
-    } catch (error) {
-      logger.error("[FolderService.create] - unexpected error");
-      throw error;
+    if (!folderCreated.length) {
+      return null;
     }
+
+    return folderCreated[0];
   }
 
   public static async find() {
-    try {
-      const folders = await db.select().from(folder);
+    const folders = await db.select().from(folder);
 
-      return folders;
-    } catch (error) {
-      logger.error("[FolderService.find] - unexpected error");
-      throw error;
-    }
+    return folders;
   }
 
   public static async findOne(id: string) {
-    try {
-      const folderFound = await db
-        .select()
-        .from(folder)
-        .where(eq(folder.id, id))
-        .limit(1);
+    const folderFound = await db
+      .select()
+      .from(folder)
+      .where(eq(folder.id, id))
+      .limit(1);
 
-      if (!folderFound.length) {
-        return null;
-      }
-
-      return folderFound[0];
-    } catch (error) {
-      logger.error("[FolderService.findOne] - unexpected error");
-      throw error;
+    if (!folderFound.length) {
+      return null;
     }
+
+    return folderFound[0];
   }
 
   public static async update(id: string, data: Folder["Update"]) {
-    try {
-      const folderUpdated = await db
-        .update(folder)
-        .set(data)
-        .where(eq(folder.id, id))
-        .returning();
+    const folderUpdated = await db
+      .update(folder)
+      .set(data)
+      .where(eq(folder.id, id))
+      .returning();
 
-      if (!folderUpdated.length) {
-        return null;
-      }
-
-      return folderUpdated[0];
-    } catch (error) {
-      logger.error("[FolderService.update] - unexpected error");
-      throw error;
+    if (!folderUpdated.length) {
+      return null;
     }
+
+    return folderUpdated[0];
   }
 
   public static async remove(id: string) {
-    try {
-      const folderDeleted = await db
-        .delete(folder)
-        .where(eq(folder.id, id))
-        .returning();
+    const folderDeleted = await db
+      .delete(folder)
+      .where(eq(folder.id, id))
+      .returning();
 
-      if (!folderDeleted.length) {
-        return null;
-      }
-
-      return folderDeleted[0];
-    } catch (error) {
-      logger.error("[FolderService.remove] - unexpected error");
-      throw error;
+    if (!folderDeleted.length) {
+      return null;
     }
+
+    return folderDeleted[0];
   }
 }
